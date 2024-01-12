fe12aa36ca288b923b29d84b6f86a5ba782b2c8d
Jose Pernia <joseapc90@gmail.com>
2024-01-11 22:42:55 -0300

    Modified HomeController and View to rezise the movies boxes

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 class HomeViewController: UIViewController {
 
     private let homeFeedTable: UITableView = {
-        let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
         return table
     }()
 
@@ -33,15 +33,24 @@ class HomeViewController: UIViewController {
 // Extensión para conformidad a los protocolos UITableViewDelegate y UITableViewDataSource
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 20
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello Word"
-        cell.backgroundColor = .red
+
+         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        
         return cell
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) ->CGFloat {
