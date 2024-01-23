269a20c6cf3a35ff7bae02b3f837b87092850f53
Jose Pernia <joseapc90@gmail.com>
2024-01-23 05:10:42 -0300

    Setting up the header viewTable

---
+++ Netflix Clone/Views/HeroHeaderUiView.swift
@@ -0,0 +1,46 @@
+//
+//  HeroHeaderUiView.swift
+//  Netflix Clone
+//
+//  Created by Jose Pernia on 23-01-24.
+//
+
+import UIKit
+
+class HeroHeaderUiView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    private func addGradient(){
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder){
+        fatalError()
+    }
+    
+}
